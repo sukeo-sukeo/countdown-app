c530ac970be9bb36aa9a9566c8c82d5b3dc6688e
sukeo-sukeo <syuke4522@gmail.com>
2022-08-07 17:28:21 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MatterInput from './components/MatterInput.vue';
+
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <MatterInput />
+    </v-main>
+  </v-app>
+</template>
---
+++ src/components/MatterInput.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-text-field
+      label="事柄"
+      placeholder="カウントダウンしたい事柄を入力してください"
+      variant="outlined"
+      ></v-text-field>
+    </v-row>
+    <v-row>
+      <v-text-field
+      label="実施日"
+      placeholder="yyyy-mm-dd"
+      variant="outlined"
+      append-icon="mdi-calendar-range"
+      ></v-text-field>
+      <v-btn class="mt-2 mx-3">
+        登録！
+      </v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<style>
+/* .v-text-field input {
+  font-size: 10px;
+  } */
+</style>
